cc1ee00fd088e2e8b1f96b71b803b20e707095ed
dcanosua <dcanosua+github@cisco.com>
2025-11-17 10:04:13 -0500

    Fix CalculosEdificio.java

--- Ejercicio4/CalculosEdificio.java
+++ Ejercicio4/CalculosEdificio.java
@@ -4,16 +4,16 @@ public class CalculosEdificio {
 
     public static double consumoTotal(Apartamento[] aptos) {
         double total = 0;
-        for (Apartamento a : aptos) {
-            total += a.getConsumoM3();
+        for (Apartamento actual : aptos) {
+            total += actual.getConsumoM3();
         }
         return total;
     }
 
     public static double valorTotal(Apartamento[] aptos) {
         double total = 0;
-        for (Apartamento a : aptos) {
-            total += a.valorPagar();
+        for (Apartamento actual : aptos) {
+            total += actual.valorPagar();
         }
         return total;
     }
@@ -22,9 +22,9 @@ public class CalculosEdificio {
         if (aptos.length == 0) return null;
 
         Apartamento mayor = aptos[0];
-        for (Apartamento a : aptos) {
-            if (a.getConsumoM3() > mayor.getConsumoM3()) {
-                mayor = a;
+        for (Apartamento actual : aptos) {
+            if (actual.getConsumoM3() > mayor.getConsumoM3()) {
+                mayor = actual;
             }
         }
         return mayor;
@@ -34,9 +34,9 @@ public class CalculosEdificio {
         if (aptos.length == 0) return null;
 
         Apartamento menor = aptos[0];
-        for (Apartamento a : aptos) {
-            if (a.getConsumoM3() < menor.getConsumoM3()) {
-                menor = a;
+        for (Apartamento actual : aptos) {
+            if (actual.getConsumoM3() < menor.getConsumoM3()) {
+                menor = actual;
             }
         }
         return menor;
